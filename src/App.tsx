dc627688b1806d5fe013b36cc3ccca5e9fbcc06b
Edson Junior <edson12.j@hotmail.com>
2021-12-13 19:48:54 -0300

    Criaçao de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import { Dashboard } from "./components/DashBoard";
 import { Header } from "./components/Header";
 import { GlobalStyled } from "./styles/global";
 
+import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+
+
+Modal.setAppElement("#root");
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+ 
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <>
-      <Header />
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyled />
-    </>
+    </TransactionsProvider>
   );
 }
